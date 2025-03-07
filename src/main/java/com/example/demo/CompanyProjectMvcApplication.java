bd9b655eec882049917e2e65a1faff7cf569ad9b
leejaeho-17 <woghdl99@naver.com>
2025-03-07 11:45:14 +0900

    first commit

---
+++ src/main/java/com/example/demo/CompanyProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyProjectMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyProjectMvcApplication.class, args);
+	}
+
+}
